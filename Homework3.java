28d1744e5cc6efead8aa0562209ab51dff71b122
Tsulunator <tsulukidzegiorgi2008@gmail.com>
2024-10-30 18:24:45 +0400

    magic3

---
+++ Homework3.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+        //1
+        /*
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        double average = (num1 + num2 + num3) / 3.0;
+        System.out.println(average);
+         */
+
+        //2
+        /*
+        int number = 8;
+        String check = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(check);
+         */
+
+        //3
+        /*
+        int num1 = 30;
+        int num2 = 20;
+        int big = (num1 > num2) ? num1 : num2;
+        System.out.println(big);
+         */
+
+        //4
+        /*
+        int number = 3456;
+        int numba = 0;
+        numba += number % 10;
+        number /= 10;
+        numba += number % 10;
+        number /= 10;
+        numba += number % 10;
+        number /= 10;
+        numba += number;
+        System.out.println(numba);
+         */
+
+        //5
+        /*
+        int a1 = 60;
+        int a2 = 70;
+        int a3 = 50;
+        if ((a1 + a2 + a3 == 180) && (a1 > 0) && (a2 > 0) && (a3 > 0)) {
+            System.out.println("Yes :)");
+        } else {
+            System.out.println("No >:(");
+        }
+         */
+    }
+}
