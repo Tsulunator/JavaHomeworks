d160f7bc4b505565a1c8f2c9ac69ff3cb150f33c
Tsulunator <tsulukidzegiorgi2008@gmail.com>
2024-12-18 18:43:08 +0400

    added

---
+++ Memkvidreoba/src/Davaleba1/Employee.java
@@ -0,0 +1,18 @@
+package Davaleba1;
+
+public class Employee extends Person{
+    private String employeeId;
+
+    public Employee(String firstName, String lastName, int age, String employeeId){
+        super(firstName, lastName, age);
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
+}
---
+++ Memkvidreoba/src/Main.java
@@ -0,0 +1,17 @@
+import Davaleba1.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //1
+        /*
+        Person person = new Person("John", "Pork", 30);
+        System.out.println("Person: " + person.getFirstName() + " " + person.getLastName() + ", Age: " + person.getAge());
+        Employee employee = new Employee("Smithy", "Smith", 28, "E123");
+        System.out.println("Employee: " + employee.getFirstName() + " " + employee.getLastName() + ", Age: " + employee.getAge() + ", Employee ID: " + employee.getEmployeeId());
+        Manager manager = new Manager("White", "Brown", 35, "E124", "M456");
+        System.out.println("Manager: " + manager.getFirstName() + " " + manager.getLastName() + ", Age: " + manager.getAge() + ", Employee ID: " + manager.getEmployeeId() + ", Manager ID: " + manager.getManagerId());
+         */
+
+        //2
+    }
+}
