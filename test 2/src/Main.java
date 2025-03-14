06dc1eb9b04be27da878a9c4a5b2fc125055273c
Tsulunator <tsulukidzegiorgi2008@gmail.com>
2025-03-14 20:11:42 +0400

    added

---
+++ test 2/src/Main.java
@@ -0,0 +1,53 @@
+import java.lang.reflect.Array;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //1
+        //a
+
+        //2
+        //b
+
+        //3
+        //a,d,b
+
+        //4
+        //c
+
+        //5
+        //a,c
+
+        //6
+        //d
+
+
+        //7
+        //c
+
+        //8
+        //b
+
+        //9
+        //b
+
+        //10
+        //c
+
+        //11
+        //c
+
+        //12
+        //b
+
+        //13
+        //a,b,c
+
+        //14
+        //d,a
+
+        //15
+        //b,c
+    }
+}
